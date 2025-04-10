85b4d83cd46e1c1e2ff8e1616c0378c92f28af53
Ahmed <hamoudi1997@yahoo.com>
2025-04-10 17:00:11 -0400

    test function added to get top 10 games based on genre

--- igdb.js
+++ igdb.js
@@ -19,6 +19,19 @@ const platformIds = {
   "Nintendo Switch": 130,
 };
 
+// genre IDs of base genres
+const genreIds = {
+  Action: 4,
+  Adventure: 5,
+  RPG: 12,
+  Shooter: 5,
+  Puzzle: 9,
+  Platformer: 8,
+  Strategy: 15,
+  Horror: 11,
+  Fighting: 6,
+};
+
 //Test function with axios to display top 10 games
 async function getGames() {
   try {
@@ -46,8 +59,6 @@ async function getGames() {
   }
 }
 
-getGames();
-
 // Test function to get top 10 games for PS5
 async function getGamesWithPlatform(platformName, platformId) {
   try {
@@ -74,11 +85,6 @@ async function getGamesWithPlatform(platformName, platformId) {
     console.error("Error getting games", error.message);
   }
 }
-
-const selectedPlatform = "PlayStation 5"; // Should be selected by user
-const selectedId = platformIds[selectedPlatform];
-
-getGamesWithPlatform(selectedPlatform, selectedId);
 
 async function getGamesWith(platformName, platformId) {
   try {
@@ -136,4 +142,44 @@ async function getOnlineCoopGames() {
   }
 }
 
-getOnlineCoopGames();
+async function getTopGamesByGenre(genreName) {
+  const genreId = genreIds[genreName];
+  try {
+    const response = await axios.post(
+      `${IGDB_CONFIG.base_url}/games`,
+      `fields name,  genres.name, total_rating;
+       where genres = (${genreId}) & total_rating != null;
+       sort total_rating desc;
+       limit 10;
+       `,
+      {
+        headers: {
+          "Client-ID": IGDB_CONFIG.client_id,
+          Authorization: `${IGDB_CONFIG.access_token}`,
+        },
+      }
+    );
+
+    if (response.data.length > 0) {
+      console.log(`Top 10 ${genreName} Games:`);
+      response.data.forEach((game, i) => {
+        console.log(`${i + 1}. ${game.name}`);
+      });
+    } else {
+      console.log(`No games found for ${genreName}.`);
+    }
+  } catch (error) {
+    console.error(`Error getting ${genreName} games:`, error.message);
+  }
+}
+
+// getGames();
+
+// const selectedPlatform = "PlayStation 5"; // Should be selected by user
+// const selectedId = platformIds[selectedPlatform];
+
+// getGamesWithPlatform(selectedPlatform, selectedId);
+
+// getOnlineCoopGames();
+
+getTopGamesByGenre("Adventure");
